f3cfd31abcb30a3fccaab06622d9139e1ed0b5a7
eddie ortega <23edortega@gmail.com>
2024-08-21 16:07:04 -0500

    program with comments

---
+++ ConsoleApp.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // this command tell what display in parenthesis
+            Console.WriteLine("Welcome to ITSE1302");
+            Console.Read();
+            // this prints out what is in parenthesis
+        }
+    }
+}
